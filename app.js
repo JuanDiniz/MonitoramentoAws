f426ee9cc171ddc4b92ad24ee04f56e519ed8344
JuanDiniz <162530319+JuanDiniz@users.noreply.github.com>
2024-08-29 15:23:38 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ClickTracker from './ClickTracker';
+
+function App() {
+    return (
+        <div className="App">
+            <ClickTracker />
+        </div>
+    );
+}
+
+export default App;
